96f02f7012326e5392ec76bdc6f68c6b90c029f0
Itachi-MacOS <warrior.006@inbox.ru>
2021-08-08 00:11:11 +0600

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Rassulzhan Turganov on 08.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
